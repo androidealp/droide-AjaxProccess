60f8388c70c723a75270d58067d2bb19c8ab80a4
Arthur Silva <arthur.fsilva96@gmail.com>
2016-10-15 12:25:29 -0300

    Add psr auto 1, 2 and 4

---
+++ ajax-proccess/DroidAjaxProgress.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Droide\AjaxProccess;
+
+/**
+ *
+ * @author Arthur Silva <arthur.fsilva96@gmail.com.br>
+ */
+class DroidAjaxProgress
+{
+
+    /*Build the library to php*/
+    public static function test()
+    {
+        echo "teste";
+    }
+
+}
