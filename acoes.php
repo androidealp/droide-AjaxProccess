ac74681e6f6f0a72cab46f11717c37ea9e2fe8d5
Arthur Silva <arthur.fsilva96@gmail.com>
2016-10-16 10:57:58 -0200

    add psr

--- acoes.php
+++ acoes.php
@@ -1,78 +1,87 @@
 <?php
 
+require_once __DIR__ .'/vendor/autoload.php';
 
-class Acoes {
-public $percent = [
-  1=>5,
-  2=>15,
-  3=>35,
-  4=>65,
-  5=>85,
-  6=>98,
-  7=>100,
-];
-public $dadosReturn = [
-  1=>"Processo etapa inicial - 5%",
-  2=>"Processo etapa importação - 15%",
-  3=>"Processo etapa dados em banco - 35%",
-  4=>"Processo etapa configurando painel - 65%",
-  5=>"Processo etapa definindo usuário - 85%",
-  6=>"Processo etapa finalizando - 98%",
-  7=>"Processo etapa finalizado - 100%",
-];
+use Droide\AjaxProccess\DroidAjaxProgress;
 
-public function ProgressBar($percent = 0, $text)
+class Acoes extends DroidAjaxProgress
 {
-$html = <<<HTML
-<p>{$text}</p>
-<div class="progress">
-  <div class="progress-bar" role="progressbar" aria-valuenow="{$percent}" aria-valuemin="0" aria-valuemax="100" style="width: {$percent}%;">
-  {$percent}%
-  </div>
-</div>
-HTML;
+    public $percent = [
+        1 => 5,
+        2 => 15,
+        3 => 35,
+        4 => 65,
+        5 => 85,
+        6 => 98,
+        7 => 100,
+    ];
 
-return $html;
-}
+    public $dadosReturn = [
+        1 => "Processo etapa inicial - 5%",
+        2 => "Processo etapa importação - 15%",
+        3 => "Processo etapa dados em banco - 35%",
+        4 => "Processo etapa configurando painel - 65%",
+        5 => "Processo etapa definindo usuário - 85%",
+        6 => "Processo etapa finalizando - 98%",
+        7 => "Processo etapa finalizado - 100%",
+    ];
 
-public function Executar()
-{
-sleep(1);
-  $dados = [
-    'mensagem'=>$this->ProgressBar($this->percent[1],$this->dadosReturn[1]),
-    'stopExec'=>0,
-    'getData'=>['processo'=>2]
-  ];
-
-  if(isset($_POST['processo']))
-  {
-    if($_POST['processo']<7)
+    public function ProgressBar($percent = 0, $text)
     {
-      $dados['getData']=[
-        'processo'=>++$_POST['processo'],
-        'stopExec'=>0
-      ];
-
-      $dados['mensagem'] = $this->ProgressBar($this->percent[$_POST['processo']],$this->dadosReturn[$_POST['processo']]);
-
-      if($dados['getData']['processo'] == 7){
-        sleep(2);
-        $dados['getData']['stopExec'] = 1;
-      }
-
+        $html = <<<HTML
+        <p>{$text}</p>
+        <div class="progress">
+            <div class="progress-bar"
+               role="progressbar"
+               aria-valuenow="{$percent}"
+               aria-valuemin="0"
+               aria-valuemax="100"
+               style="width: {$percent}%;"
+            >
+                {$percent}%
+            </div>
+        </div>
+HTML;
+        return $html;
     }
 
-  }
+    public function Executar()
+    {
+        //Test class DroidAjaxProgress
+        //$this->test();
 
+        sleep(1);
+        $dados = [
+            'mensagem'=>$this->ProgressBar(
+                $this->percent[1],
+                $this->dadosReturn[1]
+            ),
+            'stopExec'=>0,
+            'getData'=>['processo'=>2]
+        ];
 
+        if (isset($_POST['processo'])) {
+            if ($_POST['processo'] < 7) {
+                $dados['getData']=[
+                    'processo'=>++$_POST['processo'],
+                    'stopExec'=>0
+                ];
 
-  header('Content-type: application/json');
-  return json_encode($dados);
+                $dados['mensagem'] = $this->ProgressBar(
+                    $this->percent[$_POST['processo']],
+                    $this->dadosReturn[$_POST['processo']]
+                );
+
+                if ($dados['getData']['processo'] == 7) {
+                    sleep(2);
+                    $dados['getData']['stopExec'] = 1;
+                }
+            }
+        }
+        header('Content-type: application/json');
+        return json_encode($dados);
+    }
 }
-
-
-}
-
 
 $acoes = new Acoes;
 
